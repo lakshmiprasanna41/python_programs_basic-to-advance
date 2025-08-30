ddf4bd142b5eaf4a6e0ec67a1ffa1cf0e8077c62
Lakshmi Prasanna <majjilakshmiprasanna23304@gmail.com>
2025-08-30 22:03:51 +0530

    Add files via upload

---
+++ conditional_statements.py
@@ -0,0 +1,24 @@
+#conditional statements
+
+#if condition
+a=10
+if a>=10:
+    print("true")
+
+#if-else condition
+b=15
+if b>18:
+    print("you have right to vote")
+else:
+    print("you have no right to vote")
+
+
+#elif condition
+c=21
+if c>21:
+    print("You can marry")
+elif c==21:
+    print("You have to wait for more 1 year")
+else:
+    print("You can't marry")
+
