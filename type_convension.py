8cdbf4ac7584a701b593bc505de077e159e0673d
Lakshmi Prasanna <majjilakshmiprasanna23304@gmail.com>
2025-08-30 19:05:22 +0530

    Add files via upload

---
+++ type_convension.py
@@ -0,0 +1,13 @@
+#type convensions
+a=10
+print(type(a))
+
+#let's change int to float
+print(float(a))
+
+b=[1,23.4,'hii',True,50.059]
+print(type(b))
+
+#let's change list to tuple
+print(tuple(b))
+
