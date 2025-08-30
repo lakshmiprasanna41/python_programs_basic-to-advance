30fa5e48782ae065cf97a5aa2c35bdd37afdf335
Lakshmi Prasanna <majjilakshmiprasanna23304@gmail.com>
2025-08-30 22:15:19 +0530

    Add files via upload

---
+++ jumping_statements.py
@@ -0,0 +1,24 @@
+#Jumping Statements
+
+#pass--ignore
+if True:
+    pass
+
+
+
+#break--terminate
+a='Hello'
+for i in a:
+    if i=='l':
+        break
+    print(i)
+
+    
+#continue--skip(current-iteration)
+b='beautiful'
+for j in b:
+    if j=='t':
+        continue
+    print(j)
+
+
