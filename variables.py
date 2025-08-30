cd941eea8e4909f30291d36f822e74dd2b0c1067
Lakshmi Prasanna <majjilakshmiprasanna23304@gmail.com>
2025-08-30 19:04:37 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,22 @@
+#variables
+
+#example1
+a=10
+print(a)
+
+#example2
+b=1,2,3
+print(b)#get a output in tuple
+
+#example3
+p=q=r=5
+print(p,q,r)
+
+#example4
+#To know the memory address of the variable
+d=123
+print(id(d))
+
+#example5
+x,y=1
+print(x,y)#we get an error
