682defa4f0203ae6f3d4b35819b15af49d3b03d4
Lakshmi Prasanna <majjilakshmiprasanna23304@gmail.com>
2025-08-30 22:04:10 +0530

    Add files via upload

---
+++ looping_statements.py
@@ -0,0 +1,29 @@
+#looping statements----iterating statements
+
+#for loop:
+#example1
+for i in range(0,10):
+    print(i)
+
+#example2
+for j in range(0,10,2):
+    print(j)
+
+#example3
+a=[1,2,4,6,8,9,10]
+for k in a:
+    print(k)
+
+
+#while loop:
+#example1
+while True:
+    print("hi")
+
+
+#example2
+x=10
+while x<10:
+    print('hello')
+    x+=1
+
